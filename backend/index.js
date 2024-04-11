6a08b20f5b7982c55d8708298b564b7e24ad2204
AliAbdelAall <3laych@gmail.com>
2024-04-12 01:57:41 +0300

    connected node to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require("express")
 
+const { connect } = require('./config/mongoDB.config')
+
+require("dotenv").config()
+
 const app = express()
 
-app.listen(3000, () => {
+app.use(express.json())
+
+app.listen(3000, (error) => {
+  if (error) throw error
   console.log("server is running on port 3000")
+  connect()
 })
 
 app.get('/get-todo', (req, res) => {
