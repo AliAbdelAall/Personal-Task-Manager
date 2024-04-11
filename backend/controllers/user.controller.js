a1824671323ed5def33793309e1d9a0e3ba9fa56
AliAbdelAall <3laych@gmail.com>
2024-04-12 02:49:52 +0300

    added getAllUsers API in UserController

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,16 @@
+const User = require('../models/user.model')
+
+const getAllUsers = async (req, res) => {
+
+  try {
+    const users = User.find()
+    return res.json(users)
+  } catch (error) {
+    return res.status(500).send("internal server error!")
+  }
+}
+
+module.exports =
+{
+  getAllUsers,
+}
