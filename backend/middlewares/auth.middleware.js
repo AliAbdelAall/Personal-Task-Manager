f04bf02828c5f74c0025d3fd0fc0f2ca0807c9f6
AliAbdelAall <3laych@gmail.com>
2024-04-13 13:25:28 +0300

    updated auth Middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,36 +1,21 @@
 const jwt = require("jsonwebtoken")
-const User = require("../models/user.model")
+const { User } = require("../models/user.model")
 
 // check User here
 const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.header.authorization?.split(" ")[1]
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const user = await User.findByid()
+    const user = await User.findById(decode._id)
 
-
+    if (decoded) {
+      req.user = user
+    }
+    next()
   } catch (error) {
     return res.status(401).send("unauthenticated")
   }
 }
 
 module.exports = isAuthenticated
-
-// const jwt = require("jsonwebtoken");
-
-// const isAuthenticated = async (req, res, next) => {
-//   try {
-//     const token = req.header.authorization?.split(" ")[1];
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findById(decoded._id).select("-password");
-
-//     if (!user) throw new Error("User not found!");
-
-//     req.user = user;
-//   } catch (error) {
-//     return res.status(401).send("Unauthenticated");
-//   }
-// };
-
-// module.exports = isAuthenticated;
 
