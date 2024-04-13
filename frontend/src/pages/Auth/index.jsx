ab98604d16d518783c3f1a0b3f227948815ddd14
AliAbdelAall <3laych@gmail.com>
2024-04-13 15:48:01 +0300

    added Auth Component

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -1,1 +1,15 @@
-rafce
+import React from 'react'
+import { Outlet } from "react-router-dom";
+
+//style
+import "./style.css"
+
+const Auth = () => {
+  return (
+    <div className='flex center auth-container'>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Auth
