51c7ce69cbd2eb947601872e7fd01fe983b8a781
AliAbdelAall <3laych@gmail.com>
2024-04-16 00:40:56 +0300

    added task component

--- frontend/src/pages/Home/components/Task/index.jsx
+++ frontend/src/pages/Home/components/Task/index.jsx
@@ -1,1 +1,35 @@
-rafce
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Task = ({ tag, task, index }) => {
+
+  const isDragging = () => {
+    
+  }
+
+  return (
+    <Draggable draggableId={task.id} key={task.id} index={index}>
+      {(provided, snapshot) => (
+
+        <div
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        
+        ref={provided.innerRef}
+        isDragging={snapshot.isDragging}
+
+        >
+          <p>{tag}</p>
+          <h4>{task.title}</h4>
+          <p>{task.description}</p>
+          <p>{task.attachment}</p>
+
+          {provided.placeholder}
+        </div>
+
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
