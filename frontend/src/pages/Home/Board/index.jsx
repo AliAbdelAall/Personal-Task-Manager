1379557c5a484d5109fdf03e7b54f0147fddbe11
AliAbdelAall <3laych@gmail.com>
2024-04-16 00:40:21 +0300

    updated Board component

--- frontend/src/pages/Home/Board/index.jsx
+++ frontend/src/pages/Home/Board/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Column from '../components/Column';
+import "./style.css"
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const Board = () => {
-  return (
-    <div>Board</div>
-  )
-}
+  const [complete, setComplete] = useState([])
+  const [inComplete, setInComplete] = useState([])
 
-export default Board
+  return (
+    <DragDropContext>
+      <div className='flex board-wrapper'>
+
+        <Column
+        title={"To-Do"}
+        tasks={inComplete}
+        id={"1"}
+        />
+        <Column
+        title={"In-Progress"}
+        tasks={inComplete}
+        id={"2"}
+        />
+        <Column
+        title={"Done"}
+        tasks={complete}
+        id={"3"}
+        />
+
+      </div>
+    </DragDropContext>
+  );
+};
+
+export default Board;
