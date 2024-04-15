306a787b41e4d7b7fd62df2947bc09a0c1f493c5
AliAbdelAall <3laych@gmail.com>
2024-04-16 00:40:43 +0300

    added column component

--- frontend/src/pages/Home/components/Column/index.jsx
+++ frontend/src/pages/Home/components/Column/index.jsx
@@ -1,1 +1,35 @@
-rafce
+import React from 'react';
+import Task from '../Task';
+import { Droppable } from 'react-beautiful-dnd';
+
+
+const Column = ({title, tasks, id}) => {
+  return (
+    <div className='board-column'>
+      <h3>{title}</h3>
+    <Droppable droppableId={id}>
+      {(provided, snapshot) => (
+        <div
+        ref={provided.innerRef}
+        {...provided.droppableProps}
+        isDraggingOver={snapshot.isDraggingOver}
+        >
+          <Task
+          task={{
+            id: "12",
+            title: "assignment",
+            description: "make it in tie and deliver it fully functional",
+            attachment: "learn it"
+          }}
+          index={1}
+          />
+          {provided.placeholder}
+        </div>
+      )}
+
+    </Droppable>
+    </div>
+  );
+};
+
+export default Column;
