777582a427568e128d4208274785eddb9c9ea5d9
AliAbdelAall <3laych@gmail.com>
2024-04-17 20:10:29 +0300

    updated column component

--- frontend/src/pages/Home/components/Column/index.jsx
+++ frontend/src/pages/Home/components/Column/index.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import Task from '../Task';
-import { Droppable } from 'react-beautiful-dnd';
+import Button from '../../../../components/Button';
+
+import { columnSliceName } from '../../../../core/Redux/boards/columnSlice';
+import {tasksSliceName, addTask} from '../../../../core/Redux/boards/tasksSlice';
+
 
 
 const Column = ({title, tasks, id}) => {
   return (
-    <div className='board-column'>
-      <h3>{title}</h3>
-    <Droppable droppableId={id}>
-      {(provided, snapshot) => (
-        <div
-        ref={provided.innerRef}
-        {...provided.droppableProps}
-        isDraggingOver={snapshot.isDraggingOver}
-        >
-          <Task
-          task={{
-            id: "12",
-            title: "assignment",
-            description: "make it in tie and deliver it fully functional",
-            attachment: "learn it"
-          }}
-          index={1}
-          />
-          {provided.placeholder}
-        </div>
-      )}
+    <div className='flex column column-wrapper'>
+      <h3 className='column-title'>{title}</h3>
+      <div className='flex column tasks-wrapper'>
 
-    </Droppable>
-    </div>
+        <div className='task-wrapper'>
+          <h4>title</h4>
+          <p>description</p>
+          <small className='text-sm'>#tag</small>
+        </div>
+
+        <Task/>
+
+        <Button
+        text={"Add Task"}
+        />
+      </div>
+    </div> 
   );
 };
 
