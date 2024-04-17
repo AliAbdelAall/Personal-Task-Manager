da486413fbfc20a1fec3615f00f3adb80b2e3c98
AliAbdelAall <3laych@gmail.com>
2024-04-17 20:09:56 +0300

    updated Board component

--- frontend/src/pages/Home/Board/index.jsx
+++ frontend/src/pages/Home/Board/index.jsx
@@ -1,34 +1,70 @@
 import React, { useState } from 'react';
-import Column from '../components/Column';
+import { useParams, useNavigate } from 'react-router-dom';
 import "./style.css"
-import { DragDropContext } from 'react-beautiful-dnd';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { columnSliceName, addColumn } from '../../../core/Redux/boards/columnSlice';
+import { boardSliceName } from '../../../core/Redux/boards/boardsSlice';
+
+// Components
+import Column from '../components/Column';
+import AddButton from '../../../components/AddButton';
+import Button from '../../../components/Button';
+import Input from '../../../components/Input';
+import { sendRequest } from '../../../core/Utilities/remote/request';
+import { requestMethods } from '../../../core/Enums/requestMethods';
 
 const Board = () => {
-  const [complete, setComplete] = useState([])
-  const [inComplete, setInComplete] = useState([])
+
+  const { columns } = useSelector((global) => global[columnSliceName])
+  const { boards } = useSelector((global) => global[boardSliceName])
+  const { id: boardId } = useParams()
+  const navigate = useNavigate()
+  const boardColumns = columns?.filter((column) => column.boardId === boardId)
+  const board = boards?.find((board) => board._id === boardId)
+  const [columnInput, setColumnInput] = useState("")
+  const [isColumnAdd, setisColumnAdd] = useState(false)
+  
+  
+  const handleBackClick = () => {
+    navigate("/home")
+  }
+
 
   return (
-    <DragDropContext>
-      <div className='flex board-wrapper'>
+    <div className='board-container'>
+      <div className='flex board-name'>
+        <AddButton
+        text={"Back"}
+        handleClick={handleBackClick}
+        />
+        <h2>{board.name}</h2>
+      </div>
+      <div className='flex columns-wrapper'>
 
-        <Column
-        title={"To-Do"}
-        tasks={inComplete}
-        id={"1"}
-        />
-        <Column
-        title={"In-Progress"}
-        tasks={inComplete}
-        id={"2"}
-        />
-        <Column
-        title={"Done"}
-        tasks={complete}
-        id={"3"}
-        />
+        {columns?.map((column)=>(
+          <Column
+          key={column._id}
+          id={column._id}
+          title={column.title}
+          />
+      
+        ))
+        }
+        <div className='column-wrapper'>
+          {isColumnAdd && <Input
+          placeholder={"title"}
+          handleChange={(e) => setColumnInput(e.target.value)}
+          />}
+          <Button
+          text={"Add Column"}
+          
+          />
+        </div>
 
       </div>
-    </DragDropContext>
+    </div>
   );
 };
 
