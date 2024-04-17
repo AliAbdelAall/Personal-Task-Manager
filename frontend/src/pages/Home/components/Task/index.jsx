b5771146c0abc0ece09974fb78e731515d97508d
AliAbdelAall <3laych@gmail.com>
2024-04-17 20:10:43 +0300

    updated Task component

--- frontend/src/pages/Home/components/Task/index.jsx
+++ frontend/src/pages/Home/components/Task/index.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import { Draggable } from 'react-beautiful-dnd';
 
-const Task = ({ tag, task, index }) => {
 
-  const isDragging = () => {
-    
-  }
+const Task = ({ title, description, tag }) => {
 
   return (
-    <Draggable draggableId={task.id} key={task.id} index={index}>
-      {(provided, snapshot) => (
-
-        <div
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        
-        ref={provided.innerRef}
-        isDragging={snapshot.isDragging}
-
-        >
-          <p>{tag}</p>
-          <h4>{task.title}</h4>
-          <p>{task.description}</p>
-          <p>{task.attachment}</p>
-
-          {provided.placeholder}
-        </div>
-
-      )}
-    </Draggable>
+    <div className='task-wrapper'>
+      <h4>title</h4>
+      <p>description</p>
+      <small className='text-sm'>#tag</small>
+    </div> 
   );
 };
 
